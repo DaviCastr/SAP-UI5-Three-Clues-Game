3f4c8d7733597dddf1d9170e65fa2837d8ed412c
Davi Castro <davifgeo@gmail.com>
2026-07-21 17:35:33 -0300

    feat(start): create initial setup screen

--- webapp/controller/Start.controller.ts
+++ webapp/controller/Start.controller.ts
@@ -1,12 +1,29 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import UIComponent from "sap/ui/core/UIComponent";
 
-/**
- * @namespace apps.dflc.threecluesgame.controller
- */
 export default class Start extends Controller {
 
-    /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
 
+        const oConfig = new JSONModel({
+            player1: "",
+            player2: "",
+            roundTime: "20",
+            sounds: true,
+            animations: true
+        });
+
+        this.getView().setModel(oConfig, "config");
+
     }
+
+    public onStartGame(): void {
+
+        const oRouter = UIComponent.getRouterFor(this);
+
+        oRouter.navTo("Game");
+
+    }
+
 }
